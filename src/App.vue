095d6ce5e257ed2a1f96190e12096fd67361aa7d
Ogurinka Tammy Benjamin <ogurinkabenjamin@gmail.com>
2022-09-03 17:58:55 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <p>Upload a file</p>
+      <button @click="showFileSelect = !showFileSelect">Select a file</button>
+    </div>
+    <div v-show="showFileSelect">
+      <FileUpload :maxSize="1" accept="png" @file-uploaded="getUploadedData" />
+    </div>
+
+    <div v-if="fileSelected">
+      Successfully Selected file: {{ file.name }}.{{ file.fileExtention }}
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FileUpload from "@/components/FileUpload.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FileUpload,
+  },
+  data() {
+    return {
+      file: {},
+      fileSelected: false,
+      showFileSelect: false,
+    };
+  },
+  methods: {
+    getUploadedData(file) {
+      this.fileSelected = true;
+      this.showFileSelect = false;
+      this.file = file;
+    },
+  },
+};
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
